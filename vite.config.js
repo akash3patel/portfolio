c138804a4dc1e46de669a040e20d36a463b3b63b
akash3patel <147214109+akash3patel@users.noreply.github.com>
2025-07-12 23:33:14 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/portfolio/', // ← THIS IS THE FIX
   build: {
     outDir: 'dist'
   }
 });
+
